185904cfdad2bca1880a1193b7fa686103257684
mississppi <msspstudio@gmail.com>
2022-03-16 21:46:50 +0900

    create sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,69 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const sample_post = get_posts();
   return (
     <div className='wrapper'>
-      <p>hello world</p>
+      <div className="side">
+        {
+          sample_post.map(v => (
+            <li className='post'>{v}</li>
+          ))
+        }
+      </div>
+      <div className="main">
+        <p>fugafuga</p>
+      </div>
     </div>
   );
+}
+
+function get_posts(){
+  const posts = [
+    "アカウント情報",
+    "Work",
+    "MySQL情報",
+    "PHPメモ",
+    "明日やること",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+    "メモメモメモ",
+  ];
+  return posts;
 }
 
 export default App;
