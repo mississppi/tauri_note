cb1ee2854b750c72f7c4bdc3b0cc867c87538d77
mississppi <msspstudio@gmail.com>
2022-03-16 21:19:53 +0900

    first commit

---
+++ src/components/SidebarData.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const SidebarData = [
+    {
+        title: 'sample1',
+    },
+    {
+        title: 'sample2',
+    },
+    {
+        title: 'sample3',
+    },
+    {
+        title: 'sample4',
+    },
+]
